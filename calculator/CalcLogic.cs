a5cb775fc6b72616072c511ebf6355bee7d8f67e
DmytroMakarenko <hardyextrim@gmail.com>
2018-12-15 00:28:35 +0200

    Button_Sum_Logic

---
+++ calculator/CalcLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+	class CalcLogic
+	{
+		public CalcLogic()
+		{
+
+		}
+
+		public ulong Sum(ulong a, ulong b)
+		{
+			return a + b;
+		}
+
+		public ulong Result(ulong a, ulong b)
+		{
+			
+			return a;
+		}
+	}
+}
--- calculator/MainWindow.xaml.cs
+++ calculator/MainWindow.xaml.cs
@@ -27,54 +27,81 @@ namespace calculator
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+		static CalcLogic cl = new CalcLogic();
+
+		private void button1_Click(object sender, RoutedEventArgs e)
         {
-            but
+			MainText.Text += button1.Content.ToString();
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button2.Content.ToString();
+		}
 
         private void button3_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button3.Content.ToString();
+		}
 
         private void button4_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button4.Content.ToString();
+		}
 
         private void button5_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button5.Content.ToString();
+		}
 
         private void button6_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button6.Content.ToString();
+		}
 
         private void button7_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button7.Content.ToString();
+		}
 
         private void button8_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button8.Content.ToString();
+		}
 
         private void button9_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+			MainText.Text += button9.Content.ToString();
+		}
 
         private void button0_Click(object sender, RoutedEventArgs e)
         {
+			MainText.Text += button0.Content.ToString();
+		}
 
-        }
-    }
+		private void Button_clear_Click(object sender, RoutedEventArgs e)
+		{
+			MainText.Clear();
+		}
+
+		private void Buttonplus_Click(object sender, RoutedEventArgs e)
+		{
+			MainText.Text = "+";
+			if (MainText.Text == "+")
+			{
+				static ulong b = Convert.ToUInt64(MainText.Text);
+			}
+			else
+			{
+				var a = Convert.ToUInt64(MainText.Text);
+			}
+			var res = cl.Sum(a, b);
+			MainText_result.Text = res.ToString();
+		}
+
+		private void Buttonravno_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
 }
