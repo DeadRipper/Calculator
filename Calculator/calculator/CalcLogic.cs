4f25ce56b7dd471bf56be039afe17257ae1d602b
DeadRipper <hardyextrim@gmail.com>
2019-01-07 21:26:06 +0200

    Add files via upload

---
+++ Calculator/calculator/CalcLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+	class CalcLogic
+	{ 
+		public double Sum(double a, double b)
+		{
+			double c = a + b;
+			return c;
+		}
+
+		public double Raznitsa(double a, double b)
+		{
+			double c = a - b;
+			return c;
+		}
+
+		public double Mult(double a, double b)
+		{
+			double c = a * b;
+			return c;
+		}
+
+		public double Div(double a, double b)
+		{
+			double c = a / b;
+			return c;
+		}
+	}
+}
