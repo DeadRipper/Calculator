02dc16a57f4e6f27790990df52e0cd5a011ea6d3
DeadRipper <hardyextrim@gmail.com>
2019-01-07 13:43:36 +0200

    Add files via upload

---
+++ Calculator/calculator/CalcLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+	class CalcLogic
+	{ 
+		public double Sum(double a, double b)
+		{
+			double c = a + b;
+			return c;
+		}
+
+		public double Raznitsa(double a, double b)
+		{
+			double c = a - b;
+			return c;
+		}
+	}
+}
---
+++ Calculator/calculator/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace calculator
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public string Answer
+		{
+			get
+			{
+				return MainText_0.Text;
+			}
+			set
+			{
+				value = default;
+			}
+		}
+
+		public bool Input = false;
+
+		static CalcLogic cl = new CalcLogic();
+
+		private void button1_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button1.Content.ToString();
+		}
+
+		private void button2_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button2.Content.ToString();
+		}
+
+		private void button3_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button3.Content.ToString();
+		}
+
+		private void button4_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button4.Content.ToString();
+		}
+
+		private void button5_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button5.Content.ToString();
+		}
+
+		private void button6_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button6.Content.ToString();
+		}
+
+		private void button7_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button7.Content.ToString();
+		}
+
+		private void button8_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button8.Content.ToString();
+		}
+
+		private void button9_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button9.Content.ToString();
+		}
+
+		private void button0_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_1.Text += button0.Content.ToString();
+		}
+
+		private void Button_clear_Click(object sender, RoutedEventArgs e)
+		{
+			MainText_0.Clear();
+			MainText_1.Clear();
+			Operation_textbox.Clear();
+			MainText_result.Clear();
+		}
+
+		private void Buttonplus_Click(object sender, RoutedEventArgs e)
+		{
+			Operation_textbox.Text = buttonplus.Content.ToString();
+			MainText_0.Text = MainText_1.Text;
+			MainText_1.Clear();
+			if(MainText_result.Text.Contains(Answer) && MainText_1.Text.Contains(""))
+			{
+				Operation_textbox.Text = buttonplus.Content.ToString();
+				MainText_0.Text += MainText_result.Text;
+				MainText_1.Clear();
+			}
+		}
+
+		private void Buttonminus_Click(object sender, RoutedEventArgs e)
+		{
+			Operation_textbox.Text = buttonminus.Content.ToString();
+			MainText_0.Text = MainText_1.Text;
+			MainText_1.Clear();
+		}
+
+		private void Buttonravno_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				for (; ;)
+				{
+					double j = Convert.ToDouble(MainText_1.Text);
+					double i = Convert.ToDouble(MainText_0.Text);
+
+					if (Operation_textbox.Text.Contains("+"))
+					{
+						Answer = $"{i}" + "+" + $"{j}";
+						MainText_1.Clear();
+						Operation_textbox.Clear();
+						MainText_result.Text = cl.Sum(i, j).ToString();
+						break;
+					}
+					else if (Operation_textbox.Text.Contains("-"))
+					{
+						Answer = $"{i}" + "-" + $"{j}";
+						MainText_1.Clear();
+						Operation_textbox.Clear();
+						MainText_result.Text += cl.Raznitsa(i, j).ToString();
+						break;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+	}
+}
+
